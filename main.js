6c23e8a2a509b31202346c3fb0a3269b89d5cbcb
Daniel Moskal <daniel19967@gmail.com>
2020-05-09 19:11:48 +0200

    change innerHTML by JS

--- main.js
+++ main.js
@@ -1,1 +1,6 @@
-console.log('Witam na mojej stronie domowej');
+const remoteWriting = document.querySelector('.remote-writing--js');
+if (remoteWriting){
+	console.log(remoteWriting.innerHTML);
+	remoteWriting.innerHTML = 'A niech to, JS faktycznie może wstawiać tekst do DOMu, ale czy to dobre podejscie?';
+	console.log(remoteWriting.innerHTML);
+}
