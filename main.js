37af87e3c6825633df6da88a58077e1e0363fa00
Daniel Moskal <daniel19967@gmail.com>
2020-08-27 21:39:35 +0200

    greeting alerts + centered nav

--- main.js
+++ main.js
@@ -4,3 +4,26 @@ if (remoteWriting){
 	remoteWriting.innerHTML = 'A niech to, JS faktycznie może wstawiać tekst do DOMu, ale czy to dobre podejscie?';
 	console.log(remoteWriting.innerHTML);
 }
+
+function greeting(name, age) {
+	alert(`Hello ${name}, your age is ${age}`);
+}
+
+greeting('Daniel', 24);
+greeting('Daniel2', 25);
+
+const greeting2 = (personData) => {
+	alert(`Hello ${personData.name}, your age is ${personData.age}`);
+}
+const personData1 = {
+	name: 'DanielObj',
+	age: 20,
+};
+
+const personData2 = {
+	name: 'DanielObj2',
+	age: 90,
+};
+
+greeting2(personData1);
+greeting2(personData2);
